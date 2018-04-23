dbce08a1e2fa7a4edaab98ada8285705ac51378d
alexun <alexunjm@gmail.com>
2018-04-23 08:37:58 -0500

    ejemplo de suma desde inputs jquery

--- ejemplo-modificando-html/main.js
+++ ejemplo-modificando-html/main.js
@@ -4,10 +4,21 @@ console.log('está funcionando!!');
  * elemento que tiene id prueba
  * */
 $('#prueba').html('ahora si está funcionando jquery');
+
 $('#boton-prueba').click(evento => {
 	alert('hiciste click!!');
 	var parrafo = $('<p>', {text: "esto es un párrafo creado desde jquery"});
 	parrafo.appendTo("#prueba");
 	var miNombre = $('<p>', {text: "Alexander Jaramillo"});
 	miNombre.appendTo('#prueba');
+});
+
+$("#boton-texto").click(evento => {
+	var texto1 = $("#input-texto1").val();
+	var texto2 = $("#input-texto2").val();
+
+	var num1 = parseInt(texto1);
+	var num2 = parseInt(texto2);
+
+	$('#prueba').html(num1 + num2);
 });
