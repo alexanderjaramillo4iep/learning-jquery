c8ebfbbaa1341fdc8f1a7fc0592677d5703f5104
alexun <alexunjm@gmail.com>
2018-04-23 10:37:19 -0500

    corrección de orden

--- listas/main.js
+++ listas/main.js
@@ -2,8 +2,8 @@ console.log("está funcionando!!");
 $("#prueba").html("Está funcionando jquery!!");
 
 $('#crear-lista').click(evento => {
-	var elemento = $('<li>', {text: "elemento 1"});
 	var lista = $("<ul>");
+	var elemento = $('<li>', {text: "elemento 1"});
 	lista.append(elemento);
 	lista.appendTo('#prueba');
 });
