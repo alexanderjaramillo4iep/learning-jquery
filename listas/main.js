1a13389d0ef51f8f5cf2637a2267d7f8cb226d7a
alexun <alexunjm@gmail.com>
2018-04-23 10:35:08 -0500

    creando un elemento en la lista

--- listas/main.js
+++ listas/main.js
@@ -1,2 +1,9 @@
 console.log("está funcionando!!");
 $("#prueba").html("Está funcionando jquery!!");
+
+$('#crear-lista').click(evento => {
+	var elemento = $('<li>', {text: "elemento 1"});
+	var lista = $("<ul>");
+	lista.append(elemento);
+	lista.appendTo('#prueba');
+});
