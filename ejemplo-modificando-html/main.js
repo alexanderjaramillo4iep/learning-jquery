c56b7c517914ebdee28b4e3779a059af55f0cd18
alexun <alexunjm@gmail.com>
2018-04-23 08:00:18 -0500

    agregar elemento de párrafo al html desde jquery

--- ejemplo-modificando-html/main.js
+++ ejemplo-modificando-html/main.js
@@ -6,4 +6,8 @@ console.log('está funcionando!!');
 $('#prueba').html('ahora si está funcionando jquery');
 $('#boton-prueba').click(evento => {
 	alert('hiciste click!!');
+	var parrafo = $('<p>', {text: "esto es un párrafo creado desde jquery"});
+	parrafo.appendTo("#prueba");
+	var miNombre = $('<p>', {text: "Alexander Jaramillo"});
+	miNombre.appendTo('#prueba');
 });
