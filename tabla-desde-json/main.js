8d48f5870ded91d83600c1ee98156e9191fe6763
alexun <alexunjm@gmail.com>
2018-04-30 10:13:52 -0500

    estudiantes de clase y comentarios en el código

--- tabla-desde-json/main.js
+++ tabla-desde-json/main.js
@@ -1,11 +1,14 @@
 console.log("Jquery está funcionando!!");
 $("#prueba").html("Está funcionando jquery!!");
 
+/**
+ * Definición de los datos que tendrá la tabla
+ */
 const jsonPrueba = {
   data: [
-    { nombre: "andrea", apellido: "lopez" },
-    { nombre: "laura", apellido: "valencia" },
-    { nombre: "andres", apellido: "loaiza" }
+    { nombre: "adrian", apellido: "arboleda" },
+    { nombre: "daniela", apellido: "zapata" },
+    { nombre: "ana", apellido: "ruiz" }
   ]
 };
 
@@ -21,20 +24,30 @@ $("#crear-tabla").click(evento => {
    * Cabecera de la tabla
    */
   var tr = $("<tr>");
+  // para cada propiedad del primer dato
   $.each(jsonPrueba.data[0], (property, value) => {
+	  // pegamos cada columna a la fila de la cabecera
     tr.append($("<th>", { text: property }));
   });
+  // pegamos la fila (cabecera) a la tabla
   table.append(tr);
 
   /**
-   * Cuerpo de la tabla
+   * Cuerpo de la tabla (se construye con cada elemento dentro de data)
    */
   $.each(jsonPrueba.data, (index, item) => {
     tr = $("<tr>");
+    // para cada propiedad, valor; del elemento actual (el item)
     $.each(item, (property, value) => {
+		// pegamos cada propiedad como columna
       tr.append($("<td>", { text: value }));
-    });
+	});
+	// pegamos cada fila en la tabla
     table.append(tr);
   });
+  /**
+   * Decimos que el nuevo html del elemento con id="tabla"
+   * será la tabla que acabamos de crear
+   */
   $("#tabla").html(table);
 });
