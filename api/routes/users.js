a4af303b0d917368cc5831e8b3ec30a9080f7fa6
alexun <alexunjm@gmail.com>
2018-05-07 10:20:32 -0500

    consumiendo api

--- api/routes/users.js
+++ api/routes/users.js
@@ -3,14 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.status(200).send({
-    mensaje: 'hola',
-    datos: [
-      {usuario: 'trului', email: 'trului@prueba.com'},
-      {usuario: 'regnaroki', email: 'regnarok@prueba.com'},
-      {usuario: 'ltrudi', email: 'ltrudi@prueba.com'},
-    ]
-  });
+  res
+    .status(200)
+    .send({
+      mensaje: "hola",
+      datos: [
+        { usuario: "trului", email: "trului@prueba.com" },
+        { usuario: "regnaroki", email: "regnarok@prueba.com" },
+        { usuario: "ltrudi", email: "ltrudi@prueba.com" }
+      ]
+    });
 });
 
 module.exports = router;
--- tabla-jquery/main.js
+++ tabla-jquery/main.js
@@ -8,31 +8,42 @@ var lista = {
     ]
 };
 
-$('#crear-tabla').click(evento => {
-    // creamos el elemento <table>
-    var tabla = $('<table>');
-    // creamos el elemento <tr>
-    var fila = $('<tr>');
-    // pegamos a la fila un elemento <td> con texto columna x
-    fila.append($('<th>', {text: 'Nombre'}));
-    fila.append($('<th>', {text: 'Apellido'}));
-    // pegamos a la tabla la fila
-    tabla.append(fila);
+function obtenerDatos(funRta) {
+	$.ajax({ url: "http://172.29.63.51:3000/users" })
+	.done(lista => {
+		console.log(lista);
+		funRta(lista);
+	});
+}
 
-    $.each(lista.datos, function (i, dato) {
-        // creamos el elemento <tr>
-        fila = $('<tr>');
-        // pegamos a la fila un elemento <td> con texto columna x
-        fila.append($('<td>', {text: dato.nombre}));
-        fila.append($('<td>', {text: dato.apellido}));
-        // pegamos a la tabla la fila
-        tabla.append(fila);
-    });
-    /*
-    for(var i = 0; i < lista.datos.length; i++) {
-        var dato = lista.datos[i];
-    }*/
-    
-    // modificamos el html del div con id="tabla"
-    $('#tabla').html(tabla);
+$('#crear-tabla').click(evento => {
+	obtenerDatos(lista => {
+
+		// creamos el elemento <table>
+		var tabla = $('<table>');
+		// creamos el elemento <tr>
+		var fila = $('<tr>');
+		// pegamos a la fila un elemento <td> con texto columna x
+		fila.append($('<th>', {text: 'Nombre'}));
+		fila.append($('<th>', {text: 'Apellido'}));
+		// pegamos a la tabla la fila
+		tabla.append(fila);
+
+		$.each(lista.datos, function (i, dato) {
+			// creamos el elemento <tr>
+			fila = $('<tr>');
+			// pegamos a la fila un elemento <td> con texto columna x
+			fila.append($('<td>', {text: dato.nombre}));
+			fila.append($('<td>', {text: dato.apellido}));
+			// pegamos a la tabla la fila
+			tabla.append(fila);
+		});
+		/*
+		for(var i = 0; i < lista.datos.length; i++) {
+			var dato = lista.datos[i];
+		}*/
+		
+		// modificamos el html del div con id="tabla"
+		$('#tabla').html(tabla);
+	});
 });
