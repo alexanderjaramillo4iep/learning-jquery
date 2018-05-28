5caf94a66e087919fea510cd70e1e185b8b7b481
alexun <alexunjm@gmail.com>
2018-05-28 13:36:44 -0500

    librerías bootstrap y jquery locales

--- ejemplo-proyecto/routes/users.js
+++ ejemplo-proyecto/routes/users.js
@@ -1,9 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var path = require('path');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET html users listing. */
+router.get("/", function(req, res, next) {
+  res.sendFile(path.join(__dirname, '..', 'private', 'users.html'));
+});
+
+/* GET json users. */
+router.get("/get", function (req, res, next) {
+	res.status(200).send({ name: 'Usuarios', data: [
+		{ Usuario: 'Prueba 1', Contraseña: '****123' },
+		{ Usuario: 'Prueba 2', Contraseña: '****231' },
+		{ Usuario: 'Prueba 3', Contraseña: '****312' }
+	] });
+});
+
+/* GET a user. */
+router.get("/get/:id", function(req, res, next) {
+  const userId = req.params.id;
+  res.status(200).send({ response: `are you request user ${userId}?` });
+});
+
+/* SAVE a user. */
+router.post("/save", function(req, res, next) {
+  var bodyParams = req.body;
+  res.status(200).send({ response: bodyParams });
 });
 
 module.exports = router;
